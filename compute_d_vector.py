041c776de2dbe289b22009c65bde26ea86b304ed
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2021-12-25 23:00:04 +0600

    updated compute_vector_d
    while working with libri we are having cuda out of memory error .

--- compute_d_vector.py
+++ compute_d_vector.py
@@ -1,30 +1,30 @@
 # compute_d_vector.py
-# Mirco Ravanelli 
-# Mila - University of Montreal 
+# Mirco Ravanelli
+# Mila - University of Montreal
 
 # Feb 2019
 
-# Description: 
+# Description:
 # This code computes d-vectors using a pre-trained model
- 
+
 
 import os
 import soundfile as sf
-import torch
+import torch,gc
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 from dnn_models import MLP
-from dnn_models import SincNet as CNN 
+from dnn_models import SincNet as CNN
 from data_io import ReadList,read_conf_inp,str_to_bool
 import sys
 
 # Model to use for computing the d-vectors
-model_file= '/home/mirco/sincnet_models/SincNet_TIMIT/model_raw.pkl' # This is the model to use for computing the d-vectors (it should be pre-trained using the speaker-id DNN)
+model_file= 'sincnetmodel/sincnet_timit/model_raw.pkl' # This is the model to use for computing the d-vectors (it should be pre-trained using the speaker-id DNN)
 cfg_file=r'cfg/SincNet_TIMIT.cfg' # Config file of the speaker-id experiment used to generate the model
-te_lst=r'data_lists/TIMIT_test.scp' # List of the wav files to process
+te_lst=r'data_lists/libri_te.scp' # List of the wav files to process
 out_dict_file=r'd_vect_timit.npy' # output dictionary containing the a sentence id as key as the d-vector as value
-data_folder=r'timit/timit'
+data_folder=r'timitnlibri/'
 
 avoid_small_en_fr=True
 energy_th = 0.1  # Avoid frames with an energy that is 1/10 over the average energy
@@ -84,13 +84,13 @@ snt_te=len(wav_lst_te)
 try:
     os.stat(output_folder)
 except:
-    os.mkdir(output_folder) 
-    
-    
+    os.mkdir(output_folder)
+
+
 # loss function
 cost = nn.NLLLoss()
 
-  
+
 # Converting context and shift in samples
 wlen=int(fs*cw_len/1000.00)
 wshift=int(fs*cw_shift/1000.00)
@@ -110,7 +110,7 @@ CNN_arch = {'input_dim': wlen,
           'cnn_use_laynorm':cnn_use_laynorm,
           'cnn_use_batchnorm':cnn_use_batchnorm,
           'cnn_act': cnn_act,
-          'cnn_drop':cnn_drop,          
+          'cnn_drop':cnn_drop,
           }
 
 CNN_net=CNN(CNN_arch)
@@ -120,7 +120,7 @@ CNN_net.to(device)
 
 DNN1_arch = {'input_dim': CNN_net.out_dim,
           'fc_lay': fc_lay,
-          'fc_drop': fc_drop, 
+          'fc_drop': fc_drop,
           'fc_use_batchnorm': fc_use_batchnorm,
           'fc_use_laynorm': fc_use_laynorm,
           'fc_use_laynorm_inp': fc_use_laynorm_inp,
@@ -134,7 +134,7 @@ DNN1_net.to(device)
 
 DNN2_arch = {'input_dim':fc_lay[-1] ,
           'fc_lay': class_lay,
-          'fc_drop': class_drop, 
+          'fc_drop': class_drop,
           'fc_use_batchnorm': class_use_batchnorm,
           'fc_use_laynorm': class_use_laynorm,
           'fc_use_laynorm_inp': class_use_laynorm_inp,
@@ -157,29 +157,29 @@ DNN2_net.load_state_dict(checkpoint_load['DNN2_model_par'])
 CNN_net.eval()
 DNN1_net.eval()
 DNN2_net.eval()
-test_flag=1 
+test_flag=1
 
 
 d_vector_dim=fc_lay[-1]
 d_vect_dict={}
 
-   
-with torch.no_grad(): 
-    
+
+with torch.no_grad():
+
     for i in range(snt_te):
-           
+
          [signal, fs] = sf.read(data_folder+'/'+wav_lst_te[i])
-         
+
          # Amplitude normalization
          signal=signal/np.max(np.abs(signal))
-        
+
          signal=torch.from_numpy(signal).float().to(device).contiguous()
-        
-         if avoid_small_en_fr: 
+
+         if avoid_small_en_fr:
              # computing energy on each frame:
              beg_samp=0
              end_samp=wlen
-    
+
              N_fr=int((signal.shape[0]-wlen)/(wshift))
              Batch_dev=N_fr
              en_arr=torch.zeros(N_fr).float().contiguous().to(device)
@@ -193,11 +193,11 @@ with torch.no_grad():
                 count_fr_tot=count_fr_tot+1
                 if count_fr==N_fr:
                     break
-    
+
              en_arr_bin=en_arr>torch.mean(en_arr)*0.1
              en_arr_bin.to(device)
              n_vect_elem=torch.sum(en_arr_bin)
-    
+
              if n_vect_elem<10:
                  print('only few elements used to compute d-vectors')
                  sys.exit(0)
@@ -207,10 +207,10 @@ with torch.no_grad():
          # split signals into chunks
          beg_samp=0
          end_samp=wlen
-         
+
          N_fr=int((signal.shape[0]-wlen)/(wshift))
-         
-        
+
+
          sig_arr=torch.zeros([Batch_dev,wlen]).float().to(device).contiguous()
          dvects=Variable(torch.zeros(N_fr,d_vector_dim).float().to(device).contiguous())
          count_fr=0
@@ -223,20 +223,24 @@ with torch.no_grad():
              count_fr_tot=count_fr_tot+1
              if count_fr==Batch_dev:
                  inp=Variable(sig_arr)
+                 #gc.collect()
+                 #torch.cuda.empty_cache()
+                 #torch.cuda.memory_summary(device=None, abbreviated=False)
+                 print("torch:",torch.cuda.memory_summary(device=None, abbreviated=False))
                  dvects[count_fr_tot-Batch_dev:count_fr_tot,:]=DNN1_net(CNN_net(inp))
                  count_fr=0
                  sig_arr=torch.zeros([Batch_dev,wlen]).float().to(device).contiguous()
-           
+
          if count_fr>0:
           inp=Variable(sig_arr[0:count_fr])
           dvects[count_fr_tot-count_fr:count_fr_tot,:]=DNN1_net(CNN_net(inp))
-        
+
          if avoid_small_en_fr:
              dvects=dvects.index_select(0, (en_arr_bin==1).nonzero().view(-1))
-         
+
          # averaging and normalizing all the d-vectors
          d_vect_out=torch.mean(dvects/dvects.norm(p=2, dim=1).view(-1,1),dim=0)
-         
+
          # checks for nan
          nan_sum=torch.sum(torch.isnan(d_vect_out))
 
@@ -244,17 +248,13 @@ with torch.no_grad():
              print(wav_lst_te[i])
              sys.exit(0)
 
-         
+
          # saving the d-vector in a numpy dictionary
-         dict_key=wav_lst_te[i].split('/')[-2]+'/'+wav_lst_te[i].split('/')[-1]
+         #dict_key=wav_lst_te[i].split('/')[-2]+'/'+wav_lst_te[i].split('/')[-1]
+         dict_key=wav_lst_te[i]
          d_vect_dict[dict_key]=d_vect_out.cpu().numpy()
          print(dict_key)
 
 # Save the dictionary
 np.save(out_dict_file, d_vect_dict)
-         
-    
-    
-
-
-
+print("dict: ",d_vect_dict)
